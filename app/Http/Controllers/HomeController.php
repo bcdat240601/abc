6b609dbd2985ee9ca50a00e89361b333afc5f74a
bcdat240601 <78672893+bcdat240601@users.noreply.github.com>
2021-04-03 22:21:41 +0700

    update code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\dienthoai;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use DB;
 class HomeController extends Controller
 {
