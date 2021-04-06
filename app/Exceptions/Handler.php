b54ea31eac0f528e54310998bbe476b4228c102d
bcdat240601 <78672893+bcdat240601@users.noreply.github.com>
2021-04-06 19:14:04 +0700

    update code

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,39 +2,20 @@
 
 namespace App\Exceptions;
 
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
-
+use Illuminate\Auth\AuthenticationException;
+use Auth;
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
+    protected function unauthenticated($request, AuthenticationException $exception)
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+        if ($request->is('admin') || $request->is('admin/*')) {
+            return redirect()->guest('admin/login');
+        }
+        return redirect()->guest(route('login'));
     }
 }
