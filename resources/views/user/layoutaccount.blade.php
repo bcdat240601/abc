f916dc05e716ad37b15c36af7cfe24f5f2c48e21
bcdat240601 <78672893+bcdat240601@users.noreply.github.com>
2021-05-04 21:01:42 +0700

    update code

---
+++ resources/views/user/layoutaccount.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+@section('content')
+<div class="container-fluid">
+    <style>
+        .nut{
+            width: 200px;
+            height:70px;
+            margin:0 auto;
+        }
+        .rouded{
+            width: 350px;
+        }
+    </style>
+    <div class="row">
+        <div class="col-3 mt-5 mb-5" style="background-color: rgba(62, 77, 77, 0.514);padding-top:10px;">
+            <div class="rounded" >
+                <button type="button" class="btn btn-danger nut" style="background-color:rgb(184, 40, 64);"><a href="">Thông Tin Cá Nhân</a></button>
+                <button type="button" class="btn btn-danger nut" style="background-color:rgb(184, 40, 64);"><a href="">Lịch Sử Giao Dịch</a></button>
+            </div>            
+        </div>
+        <div class="col-9">
+          @yield('contentaccount')
+        </div>
+    </div>
+</div>
+    </div>
+</div>
+
+@endsection
--- resources/views/user/myaccount.blade.php
+++ resources/views/user/myaccount.blade.php
@@ -1,54 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/myaccountcss.css')}}">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container rounded bg-white mt-5 mb-5">
-        <div class="row">
-            <div class="col-md-3 border-right">
-                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">Edogaru</span><span class="text-black-50">edogaru@mail.com.my</span><span> </span></div>
-            </div>
-            <div class="col-md-5 border-right">
-                <div class="p-3 py-5">
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h4 class="text-right">Profile Settings</h4>
-                    </div>
-                    <div class="row mt-2">
-                        <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-                        <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname"></div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
-                        <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value=""></div>
-                        <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                        <div class="col-md-12"><label class="labels">Postcode</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                        <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                        <div class="col-md-12"><label class="labels">Area</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                        <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value=""></div>
-                        <div class="col-md-12"><label class="labels">Education</label><input type="text" class="form-control" placeholder="education" value=""></div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-6"><label class="labels">Country</label><input type="text" class="form-control" placeholder="country" value=""></div>
-                        <div class="col-md-6"><label class="labels">State/Region</label><input type="text" class="form-control" value="" placeholder="state"></div>
-                    </div>
-                    <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="p-3 py-5">
-                    <div class="d-flex justify-content-between align-items-center experience"><span>Edit Experience</span><span class="border px-3 p-1 add-experience"><i class="fa fa-plus"></i>&nbsp;Experience</span></div><br>
-                    <div class="col-md-12"><label class="labels">Experience in Designing</label><input type="text" class="form-control" placeholder="experience" value=""></div> <br>
-                    <div class="col-md-12"><label class="labels">Additional Details</label><input type="text" class="form-control" placeholder="additional details" value=""></div>
-                </div>
-            </div>
+@extends('user/layoutaccount')
+@section('contentaccount')
+<div class="container rounded bg-white mt-5 mb-5">
+    <div class="row">
+        <div class="col-md-3 border-right">
+            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">{{$user->name}}</span><span class="text-black-50">{{$user->email}}</span><span> </span></div>
         </div>
+        <div class="col-md-5 border-right">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h4 class="text-right">Profile Settings</h4>
+                </div>
+                <form action="{{ route('editkh')}}" method="post">
+                    @csrf
+                    <div class="row mt-2">
+                        <div class="row mt-3">                                        
+                            <input type="text" name="id" value="{{$user->id}}" style="display: none">
+                            <div class="col-md-12"><label class="labels" for="name">Tên</label><input class="form-control" type="text" name="name" value="{{$user->name}}"></div>
+                            <div class="col-md-12"><label class="labels" for="user">Tài Khoản</label><input class="form-control" type="text" name="user" value="{{$user->user}}"></div>
+                            <div class="col-md-12"><label class="labels" for="address">Địa Chỉ</label><input class="form-control" type="text" name="address" value="{{$user->address}}"></div>
+                            <div class="col-md-12"><label class="labels" for="birthday">Ngày Sinh</label><input class="form-control" type="text" name="birthday" value="{{$user->birthday}}"></div>
+                            <div class="col-md-12"><label class="labels" for="phonenumber">Số điện thoại</label><input class="form-control" type="text" name="phonenumber" value="{{$user->phonenumber}}"></div>
+                            <div class="col-md-12"><label class="labels" for="email">Email</label><input class="form-control" type="text" name="email" value="{{$user->email}}"></div>
+                           <div class="col-md-12"><input type="submit" value="Lưu">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>                    
     </div>
-    </div>
-    </div>
-</body>
-</html>
+</div>
+
+@endsection
